dddec9fe1321faef5fc53c90eb258f93174f7d0a
Greg Swallow <gswallow@gmail.com>
2016-12-18 17:51:24 -0500

    Fix SNS topic and whatever's wrong with max_poll_retries.

--- sparkleformation/vpn.rb
+++ sparkleformation/vpn.rb
@@ -1,6 +1,6 @@
 ENV['sg']                 ||= 'vpn_sg'
 ENV['chef_run_list']      ||= 'role[base],role[openvpn_as]'
-ENV['notification_topic'] ||= "#{ENV['org']}-#{ENV['environment']}-deregister-chef-node"
+ENV['notification_topic'] ||= "#{ENV['org']}_#{ENV['environment']}_deregister_chef_node"
 
 SparkleFormation.new(:vpn, :provider => :aws).load(:base, :chef_base, :ssh_key_pair, :trusty_ami).overrides do
   description <<"EOF"
